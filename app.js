3d441893cfb36c679d899be431c89353e574e3cf
AntoineAssanvo <62053287+AntoineAssanvo@users.noreply.github.com>
2020-09-30 17:44:35 +0200

    daf

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 "user scrict";
-$(document).ready(function () {});
+$(document).ready(function () {
+  function square() {
+    let oui = document.createElement("div");
+    oui.classList.add("square");
+    document.querySelector("section").appendChild(oui);
+  }
+  for (i = 0; i > 17; i++) {}
+  square();
+});
